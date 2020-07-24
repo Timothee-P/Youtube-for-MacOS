a82c1157016130400ef178ebe3474300c13afac0
Timothee-P <33151156+Timothee-P@users.noreply.github.com>
2020-07-24 19:46:02 +0200

    New adsblock

--- app/main/index.js
+++ app/main/index.js
@@ -2,17 +2,21 @@ const { app } = require("electron");
 
 const { Menu } = require("./menu.js");
 const { Cookies } = require("./cookies.js");
-const { AdsBlock } = require("./adsBlock.js");
 const { Ipc } = require("./ipc.js");
 const { Window } = require("./window.js");
+const { session } = require("electron");
+var { ElectronBlocker } = require('@cliqz/adblocker-electron');
+var fetch = require('cross-fetch'); // required 'fetch'
 
 app.allowRendererProcessReuse = true; // Default value false is deprecated, it will change to "true" in Electron 9
 
-app.on("ready", () => {
+app.on("ready", async () => {
 	Menu.load();
 	Cookies.load(true);
 	app.dock.show();
-	AdsBlock.start();
+	ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+		blocker.enableBlockingInSession(session.defaultSession);
+	});
 	Ipc.load();
 
 	Window.init();
